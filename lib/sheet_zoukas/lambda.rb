be71f987db3b11c62e300f1679bff3443af5dbe2
eebbesen <eebbesen.git@gmail.com>
2024-10-18 21:08:03 -0500

    more logging

--- lib/sheet_zoukas/lambda.rb
+++ lib/sheet_zoukas/lambda.rb
@@ -10,19 +10,21 @@ module SheetZoukas
     class Error < StandardError; end
 
     def self.lambda_handler(event:, context:)
+      SheetZoukas::Lambda::Logger.log('DEBUG', "event: #{event}")
+      SheetZoukas::Lambda::Logger.log('DEBUG', "context: #{context}")
+
       sheet_id = event[:sheet_id]
       tab_name = event[:tab_name]
       range = event[:range]
-
-      SheetZoukas::Lambda::Logger.log('DEBUG', "event: #{event}")
-      SheetZoukas::Lambda::Logger.log('DEBUG', "context: #{context}")
 
       call_sheet(sheet_id, tab_name, range)
     end
 
     private_class_method def self.call_sheet(sheet_id, tab_name, range = nil)
+
       SheetZoukas.retrieve_sheet_json(sheet_id, tab_name, range)
     rescue StandardError => e
+      SheetZoukas::Lambda::Logger.log('DEBUG', "sheet_id: #{sheet_id}\ntab_name: #{tab_name}\nrange: #{range}")
       SheetZoukas::Lambda::Logger.log('ERROR', "call_sheet:\n #{e}")
       raise e
     end
